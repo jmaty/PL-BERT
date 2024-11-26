60752bb27588f5e61991b3b38e9130a1aa6f80a2
Jindrich Matousek <jindrich.matousek@gmail.com>
2024-11-26 13:07:42 +0100

    Adapt for word pieces Refactor Add check for unsupported phonemes

--- phonemize.py
+++ phonemize.py
@@ -2,40 +2,46 @@ import numpy as np
 # import string
 # from text_normalize import normalize_text, remove_accents
 
-def phonemize(text, phonemizer, tokenizer, punctuation):
+def phonemize(text, phonemizer, tokenizer, punctuation, text_cleaner):
     # print('SENT:', text)
     text = text.replace('-', ' ')
 
     try:  # Phonify input text (sentence)
         phonemizer.ssml_parse(text.lower())
     except RuntimeError as e:
-        print('[!] Phonemizing failed in tts_tool => skipping\n')
-        print()
-        raise
-    
+        raise RuntimeError(f'Phonemizing failed in tts_tool\n{e}') from e
+
     ph_sentences = list(phonemizer.to_sentences_phon())
     if len(ph_sentences) > 1:
-        print(f'[!] Phonemizing returned multiple sentences => using only the first one:\n{ph_sentences[0]}')
-    # assert len(ph_sentences) == 1, f'Single sentence expected at the output of G2P:\n{ph_sentences}'
-    # print('PHS: ', ph_sentences[0])
+        print(f'[!] Phonemizing returned multiple sentences => \
+              using only the first one:\n{ph_sentences[0]}')
+    ph_sentence = ph_sentences[0]
+
+    if not text_cleaner.check(ph_sentence):
+        raise ValueError(f'Unsupported phoneme detected in sentence:\n{ph_sentence}')
 
     words = split_punctuation(text.split(), punctuation)
     # print('WRD: ', words)
-    ph_words = split_punctuation(ph_sentences[0].split(), punctuation)
+    ph_words = split_punctuation(ph_sentence.split(), punctuation)
     # print('PHW: ', ph_words)
 
     # Get word pieces and their IDs; append IDs to the sentence list
     # Go through all words in a sentence
-    word_pieces_ids = [tokenizer.encode(word, add_special_tokens=False) for word in words]
+    # word_pieces_ids = [tokenizer.encode(word, add_special_tokens=False) for word in words]
+
+    word_pieces_ids, phword_pieces = match_words_and_phwords(words, ph_words, tokenizer)
 
     ## Get word piece IDs and phonetic pieces
     # inp_ids, inp_phwords = match_words_and_phwords(words, ph_words, tokenizer)
 
     # print("INP: ", word_pieces_ids)
-    # print("OUT: ", ph_words)
+    # print("OUT: ", phword_pieces)
 
-    assert len(word_pieces_ids) == len(ph_words), f'Different lengths:\nwords={len(word_pieces_ids)}: {word_pieces_ids}\nphwords={len(ph_words)}: {ph_words}'
-    return {'input_ids' : word_pieces_ids, 'phonemes': ph_words}
+    assert len(word_pieces_ids) == len(phword_pieces), \
+           f'Different lengths:\nwords={len(word_pieces_ids)}: {word_pieces_ids}\
+            \nphwords={len(phword_pieces)}: {phword_pieces}'
+
+    return {'input_ids' : word_pieces_ids, 'phonemes': phword_pieces}
 
 
 def split_punctuation(words, punctuations):
@@ -60,10 +66,8 @@ def match_words_and_phwords(words, ph_words, tokenizer):
     # Go through all words in a sentence
     for word, ph_word in zip(words, ph_words):
         word_pieces_ids, ph_pieces = match_word_and_phonemes(word, ph_word, tokenizer)
-        # all_w_piece_ids.extend(word_pieces_ids)
-        all_w_piece_ids.append(word_pieces_ids)
-        # all_ph_pieces.extend(ph_pieces)
-        all_ph_pieces.append(ph_pieces)
+        all_w_piece_ids.extend(word_pieces_ids)
+        all_ph_pieces.extend(ph_pieces)
 
     return all_w_piece_ids, all_ph_pieces
 
@@ -140,31 +144,32 @@ def split_ph_word(word_pieces, ph_word):
         else:
             orig_word += piece
 
-    print("Original word:     ", orig_word)
+    # print("Original word:     ", orig_word)
 
     # Krok 2: Použití Needleman-Wunschova algoritmu
     aligned_word, aligned_phonetic = needleman_wunsch(orig_word, ph_word)
-    print("Word pieces:       ", word_pieces)
-    print("Aligned word:      ", aligned_word)
-    print("Aligned phon. word:", aligned_phonetic)
+    # print("Word pieces:       ", word_pieces)
+    # print("Aligned word:      ", aligned_word)
+    # print("Aligned phon. word:", aligned_phonetic)
 
     # Krok 3: Mapování word pieces na fonetické části
     ph_pieces = []
     idx = 0
     for piece in word_pieces:
-        piece_clean = piece[2:] if piece.startswith('##') else piece
-        piece_len = len(piece_clean)
+        prefix = '##' if piece.startswith('##') else ''
+        piece_clean = piece[2:] if prefix else piece
 
-        # Extrahujte odpovídající část z zarovnaného fonetického přepisu
+        # Extrahujte odpovídající část ze zarovnaného fonetického přepisu
         ph_piece = ''
         count = 0
-        while count < piece_len and idx < len(aligned_word):
+        while count < len(piece_clean) and idx < len(aligned_word):
             if aligned_word[idx] != '-':
                 count += 1
             if aligned_phonetic[idx] != '-':
                 ph_piece += aligned_phonetic[idx]
             idx += 1
-        ph_pieces.append(ph_piece)
+        # Collect phon. word pieces including "subword" prefix
+        ph_pieces.append(prefix+ph_piece)
 
     return ph_pieces
 
